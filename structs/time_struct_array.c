986d1cdbc9fac13adb2151ebc7b8392f5f6e2333
srikanthpragada <srikanthpragada@gmail.com>
2020-10-29 17:57:43 +0530

    29-OCT

---
+++ structs/time_struct_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct time
+{
+  int h,m,s;
+};
+
+
+void main()
+{
+ struct time times[5];
+ int i;
+
+    srand(time(0));
+    for(i=0; i < 5; i ++)
+    {
+       times[i].h = rand() % 24;
+       times[i].m = rand() % 60;
+       times[i].s = rand() % 60;
+       printf("%02d:%02d:%02d\n", times[i].h, times[i].m, times[i].s);
+    }
+
+}
+
